c85a79880c5197549ce7f78c11905590ec41e616
Fernando Acevedo <j.fernando.acevedo.b@gmail.com>
2021-10-08 22:30:19 +0000

    Include Destinos component

--- ang-hm/src/app/app.module.ts
+++ ang-hm/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SaluteComponent } from './salute/salute.component';
+import { DestinosComponent } from './destinos/destinos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SaluteComponent
+    SaluteComponent,
+    DestinosComponent
   ],
   imports: [
     BrowserModule,
